57c0237251f08bd186e82fdd510b5da6bbd1591f
theo auffeuvre <theo.auffeuvre@gmail.com>
2023-02-27 16:40:31 +0100

    creating new branch

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_131333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "activities", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.string "poster_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,19 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_131333) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-   end
-   
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_124948) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "activities", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "location"
-    t.string "poster_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
